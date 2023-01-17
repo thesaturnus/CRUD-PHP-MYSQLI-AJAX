0a714c5c641b12de373c1c88706d4e6b30baf423
Fay <makkarennu14@gmail.com>
2023-01-17 15:12:04 +0700

    1701

---
+++ hapus-siswa.php
@@ -0,0 +1,14 @@
+<?php
+include('koneksi.php');
+
+//get id
+$id = $_GET['id'];
+$query = "DELETE FROM tbl_siswa WHERE id_siswa = '$id'";
+
+if($connection->query($query)) {
+    header("location: index.php");
+} else {
+    echo "DATA GAGAL DIHAPUS!";
+}
+
+?>
